9e357774e5ee92074d6247aa6ae6dde01eaa4eff
Sam Newman <acutecoder@gmail.com>
2012-09-20 00:20:52 +0100

    a few small changes

--- public/js/dash.js
+++ public/js/dash.js
@@ -49,50 +49,60 @@ $(function() {
 
 		events : {
 			'click #write_blog_switch' : 'switch',
-			
-		}
+			'click #write_enter' : 'enter',
+			'click #write_clear' : 'clear'
+ 		}
 
 		, initialize : function () {
 
-			this.template = new EJS({ url : '/views/write_blog.ejs' });
-			this.model = new Blog;
-			this.el = '#write_blog';
-			this.$el = $( this.el );
-			this.container = '#write_blog_holder';
+			this.template 	= new EJS({ url : '/views/write_blog.ejs' });
+			this.model 		= new Blog;
+			this.el 		= '#write_blog';
+			this.$el 		= $( this.el );
+			this.container 	= '#write_blog_holder';
 			this.$container = this.$( this.container );
-			this.open = false;
+			this.open 		= false;
 		}
 
 		, render : function() {
-
 			var ta = this.$container
 					.html( this.template.render( this.model.toJSON() ) )
 					.find( '#write_blog_entry' );
 			Autogrow.init( ta );
 		}
 
-		, clear : function() {
-
+		, close : function() {
 			this.$container.html('');
 		}
 
 		, switch : function( ab ) {
-			
 			event.preventDefault();
 			//console.log( ab.target.getAttribute('data-hmm') );
-
 			if( !this.open ) {	//	tab is closed - OPEN
-
 				this.render();
 				this.$container.addClass( 'open' )
+				this.title 		= '#write_blog_title';
+				this.$title 	= $( this.title );
+				this.entry 		= '#write_blog_entry';
+				this.$entry 	= $( this.entry );
 			}
 			else if( this.open ) {	//	tab is open - CLOSE
-
-				this.clear();
+				this.close();
 				this.$container.removeClass( 'open' );
 			}
 
 			this.open = !this.open;
+		}
+
+		, enter : function () {
+			event.preventDefault();
+			console.log('enter');
+		}
+
+		, clear : function () {
+			event.preventDefault();
+			this.$title.val('');
+			this.$entry.text('');
 		}
 	}),
 
